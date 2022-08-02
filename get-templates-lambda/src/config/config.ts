92c14e69f136d9b6af6aee715059f831439f6b99
atmecsneeleshmekala <101714950+atmecsneeleshmekala@users.noreply.github.com>
2022-08-02 16:11:29 +0530

    Update config.ts

--- get-templates-lambda/src/config/config.ts
+++ get-templates-lambda/src/config/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    LOG_GROUP_NAME: "Acertus-Dev-Template-Design",
-    LOG_STREAM_NAME: "Acertus-Dev-Get-Templates-Stream",
+    LOG_GROUP_NAME: "",
+    LOG_STREAM_NAME: "",
     ACCESS_KEY_ID_AWS: "",
     SECRET_ACCESS_KEY_AWS: "",
     REGION_AWS: "us-east-1",
